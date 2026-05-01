57df59f9ddfb95262e6af944d6c2f86c371f718f
Carina Diesel <cdiesel@dvtsoftware.com>
2026-05-01 14:51:37 +0200

    chore: Added sidebar for navigation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { Sidebar } from './shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { PanelMenuModule } from 'primeng/panelmenu';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [PanelMenuModule, ButtonModule],
+  templateUrl: './sidebar.component.html',
+  standalone: true,
+})
+export class Sidebar {
+  items = [
+    {
+      label: 'Home',
+    },
+    {
+      label: 'Artists',
+    },
+    {
+      label: 'Playlists',
+    },
+  ];
+}
