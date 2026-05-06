e4feb66075f87ca5b8eae07164a9e1a45d7c8c66
Carina Diesel <cdiesel@dvtsoftware.com>
2026-05-06 08:28:17 +0200

    feat: Added track list component

---
+++ src/app/shared/utils/format-duration.ts
@@ -0,0 +1,6 @@
+export const formatDuration = (durationInSeconds: number): string => {
+  const minutes = Math.floor(durationInSeconds / 60);
+  const seconds = durationInSeconds % 60;
+
+  return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+};
