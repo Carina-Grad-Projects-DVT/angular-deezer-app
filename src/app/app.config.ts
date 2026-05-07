279f20e99cb13769d22a06f90a35e8e2ddbc27f6
Carina Diesel <cdiesel@dvtsoftware.com>
2026-05-07 11:49:30 +0200

    feat: Setup auth0

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { provideAuth0 } from '@auth0/auth0-angular';
 import { providePrimeNG } from 'primeng/config';
 import Material from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +15,13 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideHttpClient(withFetch()),
     provideRouter(routes),
+    provideAuth0({
+      domain: environment.auth0.domain,
+      clientId: environment.auth0.clientId,
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
     provideClientHydration(withEventReplay()),
     providePrimeNG({
       theme: {
